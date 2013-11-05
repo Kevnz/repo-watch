a666d0cb2ce3ea171beae3a87898965b9c7dde35
Kevin Isom <kevin.isom@gmail.com>
2013-11-06 09:07:13 +1300

    modified view for language stuff

--- js/views/repo-list-view.js
+++ js/views/repo-list-view.js
@@ -1,10 +1,10 @@
 YUI.add('repo-list-view', function (Y) {
  
     //this has a dependency on nvd3 but it is current global
-    Y.RepoListView = Y.Base.create('repoListView', Y.CollectionView, [], {
+    Y.RepoListView = Y.Base.create('repoListView', Y.View, [], {
         events: {
             '.lang-button': {
-                click : 'languageClick'
+                click: 'filterLanguages'
             },
             'th a' : {
                 click: 'listSort'
@@ -14,7 +14,8 @@ YUI.add('repo-list-view', function (Y) {
             if(options) {
 
             }
-
+            var list = this.get('modelList');
+            list.after(['add', 'remove', 'reset'], this.render, this);
             this.publish('repoSelected', {
                 broadcast: true,
                 bubbles: true,
@@ -41,10 +42,47 @@ YUI.add('repo-list-view', function (Y) {
             };
             this.get('modelList').sort({descending: this._sort.descending});
         },
-        onRender: function () {
-            var self = this;
+        filterLanguages: function (e){
+            Y.log('filter clicked');
+            e.preventDefault();
+            var lang = e.currentTarget.getData('lang');
+            this.isFiltered = true;
+            this.currentFilter = lang;
+            this.render();
+        },
+        currentFilter: null,
+        isFiltered: false,
+        initializer: function () {
+            var list = this.get('modelList');
+            list.after(['add', 'remove', 'reset'], this.render, this);
+        },
+        render: function () {
+            var modelList = this.get('modelList');
+            var languages = modelList.getLanuages();
+            var repos = modelList.toJSON(); 
+            Y.log(this.isFiltered);
+            if (this.isFiltered) {
+                var langFilter = this.currentFilter;
+                repos = modelList.filter(function (item){
+                    return item.get('language') === langFilter;
+                }).toJSON();
+
+            }
+            var container = this.get('container'),
+                templateNode = Y.one(this.template),
+                source = templateNode.getHTML(),
+                compiledTemplate = Y.Handlebars.compile(source),
+                data = { items: modelList.toJSON(), langs: languages },
+                html = compiledTemplate(data);
+            container.setHTML(html);
+            Y.log(languages);
+            if (!container.inDoc()) {
+                Y.one('body').append(container);
+            }
+            this.fire('render');
+            return this;
         },
         _sort : {attr: '', descending: false }  
 
     });
-}, '0.0.1', { requires:['collection-view']});
+}, '0.0.1', { requires:['view']});
