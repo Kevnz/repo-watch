c8f17af263df46245da95b53d8e8a222d88504e5
Kevin Isom <kevin.isom@gmail.com>
2016-01-03 17:16:36 +1300

    repo list stuff

--- app/components/repository-list.js
+++ app/components/repository-list.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Search from 'react-search';
+const _ = require('lodash');
 
-const RepoItem = (props) =>  <div><a href={props.html_url}>{props.name}</a></div>;
+const RepoItem = (props) => <div key={props.key}><a href={props.html_url}>{props.name}</a>: <span>{props.description || 'No description'}</span></div>;
 
 export default class RepositoryList extends React.Component {
   static propTypes = {
@@ -9,21 +9,45 @@ export default class RepositoryList extends React.Component {
   }
   constructor(props) {
     super(props);
+    this.state = { searchTerm : '',searchLanguage: '' };
   }
   static displayName = "RepositoryList"
-  handleChange() {
-    console.log('handleChange');
+  handleChange(e) {
+    if(e.target.value.length > 2) {
+      this.setState({searchTerm: e.target.value});
+    }
   }
   render() {
     const { repos } = this.props;
-    const repoList = repos.map((repo, index) => <RepoItem {...repo} />);
+
+    const filteredList = repos.filter((item) => {
+      if (this.state.searchLanguage.length !== 0) {
+        return item.language === this.state.searchLanguage;
+      }
+      if (this.state.searchTerm.length === 0) return true;
+      if (item.full_name && item.full_name.toLowerCase().indexOf(this.state.searchTerm) > -1) return true;
+      if (item.description && item.description.toLowerCase().indexOf(this.state.searchTerm) > -1) return true;
+      if (item.language && item.language.toLowerCase().indexOf(this.state.searchTerm) > -1) return true;
+      if (item.language && item.language.toLowerCase().indexOf(this.state.searchTerm) > -1) return true;
+      return false;
+    });
+    const languages = filteredList.map(item => item.language);
+    const langed = _.uniq(languages);
+    console.log('uniq languages', langed);
+    const langButtons = langed.map(language => <button key={language} onClick={()=>::this.setState({searchTerm:this.state.searchTerm, searchLanguage: language })}>{language || 'No Language'}</button>)
+    const repoList = filteredList.map((repo, index) => <RepoItem {...repo}  key={'repo-item' + index}/>);
     return (<section>
-      <Search
-          items={repos}
-          keys={['name', 'language']}
-          placeholder='Search'
-          onChange={::this.handleChange} />
       <div>
+        <input
+            placeholder='Search'
+            onChange={::this.handleChange} />
+        <button onClick={()=>::this.setState({searchTerm : '',searchLanguage: ''})}>Clear</button>
+        </div>
+      <div>
+        {langButtons}
+      </div>
+      <div>
+        <h3>Results Total: {filteredList.length}</h3>
         {repoList}
       </div>
     </section>);
