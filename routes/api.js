1e3f03f606411001cdeb0df193f7b493653ea31b
Kevin Isom <kevin.isom@gmail.com>
2016-01-03 17:17:18 +1300

    cache stuff

--- routes/api.js
+++ routes/api.js
@@ -3,25 +3,31 @@ var router = express.Router();
 var config = require('xtconf')();
 /* GET users listing. */
 router.get('/:user', function(req, res, next) {
-    var Github = require('node-github-api');
-    var github = new Github({version: "3.0.0"});
-    github.githubApi.authenticate({
-        type: "basic",
-        username: config.get('GithubUser'),
-        password: config.get('GithubPassword')
-    });
-    console.log(github);
-    github.getStars(req.params.user)
-    .then(function(stars) {
-      console.dir(stars.length);
-      res.header('Content-Type', 'application/json');
-      res.send(stars);
-    })
-    .catch((err) => {
-        console.log('boom');
-        console.log(err);
-    });
-
+    var cached = req.cache.get('user-' + req.params.user);
+    if (cached) {
+        res.send(cached);
+    } else {
+        var Github = require('node-github-api');
+        var github = new Github({version: "3.0.0"});
+        github.githubApi.authenticate({
+            type: "basic",
+            username: config.get('GithubUser'),
+            password: config.get('GithubPassword')
+        });
+        console.log(github);
+        github.getStars(req.params.user)
+        .then(function(stars) {
+            req.cache.set('user-' + req.params.user, stars);
+          console.dir(stars.length);
+          res.header('Content-Type', 'application/json');
+          res.send(stars);
+        })
+        .catch((err) => {
+            console.log('boom');
+            console.log(err);
+            res.send(require('../dummy-data.json'));
+        });
+    }
 });
 
 module.exports = router;
