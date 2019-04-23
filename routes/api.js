878d07d5f68604520f0aec6ba9005637ca14a0f0
Kevin Isom <kevin@isom.nz>
2019-04-23 19:10:14 +1200

    Setup some new stuff

--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,86 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
+const url = require('url')
+const Octokit = require('@octokit/rest')
 
-/* GET users listing. */
-router.get('/:user', function(req, res, next) {
-    var cached = req.cache.get('user-' + req.params.user);
-    if (cached) {
-        res.send(cached);
-    } else {
-        var Github = require('node-github-api');
-        var github = new Github({version: "3.0.0"});
+var github = new Octokit();
+const getPageFromLink = function (metaLinks) {
+  if (!metaLinks) return 0;
+  var parsed = metaLinks.split(',');
+  var next = parsed[0].split(';');
+  //var last = parsed[1].split(';');
+  var cleanedNextLink = next[0].replace('<', '').replace('>', '');
+  var pLink = url.parse(cleanedNextLink, true);
+  return pLink.query.page;
+};
 
-        console.log(github);
-        github.getStars(req.params.user)
-        .then(function(stars) {
-            req.cache.set('user-' + req.params.user, stars);
-          console.dir(stars.length);
-          res.header('Content-Type', 'application/json');
-          console.log('stars', stars)
-          fs.writeFileSync('stars.json', JSON.stringify(stars, null, 2))
-          res.send(stars);
-        })
-        .catch((err) => {
-            console.log('boom');
-            console.log(err);
-            res.send(require('../dummy-data.json'));
-        });
-    }
+const getStars = async (username, page = 1) => github.activity.listReposStarredByUser({ username, page, per_page: 100 })
+
+const reaper = async (username) => {
+  const stars = []
+  const {data, status, headers } = await getStars(username)
+  stars.push(...data)
+  const page = getPageFromLink(headers.link)
+  if (page > 0) {
+    const p2 = await getStars(username, page)
+  }
+}
+
+const doWhile = async (action, condition) => {
+	const actionResult = await action();
+
+	if (condition(actionResult)) {
+		return doWhile(action, condition)
+	}
+}
+
+const doWhile = async (action, condition) => {
+	const actionResult = await action();
+
+	if (condition(actionResult)) {
+		return doWhile(action, condition)
+	}
+}
+
+
+router.get('/:user', async function (req, res, next) {
+  var cached = req.cache.get('userz-' + req.params.user);
+  if (cached) {
+    res.send(cached);
+  } else {
+
+    console.log('req.params.user', req.params)
+
+    const stars = []
+
+    await doWhile()
+    getStars(req.params.user)
+
+      .then(function ({ data, status, headers, ...others }) {
+
+        console.info('status', status)
+        console.info('headers', headers)
+        console.info('others', others)
+        stars.push(...data)
+        const page = getPageFromLink(headers.link)
+        console.info('page', page)
+        if (page > 0) {
+          const
+        }
+        req.cache.set('user-' + req.params.user, data);
+        console.dir(data.length);
+        res.header('Content-Type', 'application/json');
+
+        fs.writeFileSync('stars.json', JSON.stringify(data, null, 2))
+        res.send(data);
+      })
+      .catch((err) => {
+        console.log('boom');
+        console.log(err);
+        res.send(require('../dummy-data.json'));
+      });
+  }
 });
 
 module.exports = router;
