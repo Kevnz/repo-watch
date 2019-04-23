ac619ff13c6842d8ac410ad5984e236ea02a9681
Kevin Isom <kevin@isom.nz>
2019-04-23 21:01:01 +1200

    Updated some things

--- routes/api.js
+++ routes/api.js
@@ -1,85 +1,69 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const fs = require('fs');
 const url = require('url')
 const Octokit = require('@octokit/rest')
-
-var github = new Octokit();
-const getPageFromLink = function (metaLinks) {
+const parse = require('parse-link-header');
+const github = new Octokit({
+  auth: process.env.GITHUB_API
+});
+const getNextPageFromLink = function (metaLinks) {
   if (!metaLinks) return 0;
-  var parsed = metaLinks.split(',');
-  var next = parsed[0].split(';');
-  //var last = parsed[1].split(';');
-  var cleanedNextLink = next[0].replace('<', '').replace('>', '');
-  var pLink = url.parse(cleanedNextLink, true);
-  return pLink.query.page;
-};
 
+  var parsed = parse(metaLinks);
+  console.log(parsed);
+  if (!parsed.next) return ;
+  return parseInt(parsed.next.page, 10)
+};
+const getLastPageFromLink = function (metaLinks) {
+  if (!metaLinks) return 0;
+
+  var parsed = parse(metaLinks);
+  if (!parsed.last) return 0;
+  return parseInt(parsed.last.page || 100, 10);
+};
 const getStars = async (username, page = 1) => github.activity.listReposStarredByUser({ username, page, per_page: 100 })
 
-const reaper = async (username) => {
-  const stars = []
-  const {data, status, headers } = await getStars(username)
-  stars.push(...data)
-  const page = getPageFromLink(headers.link)
-  if (page > 0) {
-    const p2 = await getStars(username, page)
-  }
-}
+const checkPageLink = () => {
 
-const doWhile = async (action, condition) => {
-	const actionResult = await action();
-
-	if (condition(actionResult)) {
-		return doWhile(action, condition)
-	}
-}
-
-const doWhile = async (action, condition) => {
-	const actionResult = await action();
-
-	if (condition(actionResult)) {
-		return doWhile(action, condition)
-	}
 }
 
 
 router.get('/:user', async function (req, res, next) {
-  var cached = req.cache.get('userz-' + req.params.user);
+  const stars = []
+  const reaper = async (username, page = 1) => {
+
+  const {data, status, headers } = await getStars(username, page)
+  const lastPage = getLastPageFromLink(headers.link)
+  stars.push(...data)
+
+  const pager = getNextPageFromLink(headers.link)
+
+	if (pager <= lastPage) {
+    try {
+      return reaper(username, pager)
+    } catch (err) {
+      console.error(err)
+      return
+    }
+  }
+  return
+}
+  var cached = req.cache.get('user-' + req.params.user);
   if (cached) {
     res.send(cached);
   } else {
+    // const stars = []
 
-    console.log('req.params.user', req.params)
+    await reaper(req.params.user)
 
-    const stars = []
-
-    await doWhile()
-    getStars(req.params.user)
-
-      .then(function ({ data, status, headers, ...others }) {
-
-        console.info('status', status)
-        console.info('headers', headers)
-        console.info('others', others)
-        stars.push(...data)
-        const page = getPageFromLink(headers.link)
-        console.info('page', page)
-        if (page > 0) {
-          const
-        }
-        req.cache.set('user-' + req.params.user, data);
-        console.dir(data.length);
+        req.cache.set('user-' + req.params.user, stars);
+        console.dir(stars.length);
         res.header('Content-Type', 'application/json');
 
-        fs.writeFileSync('stars.json', JSON.stringify(data, null, 2))
-        res.send(data);
-      })
-      .catch((err) => {
-        console.log('boom');
-        console.log(err);
-        res.send(require('../dummy-data.json'));
-      });
+        fs.writeFileSync('stars.json', JSON.stringify(stars, null, 2))
+        res.send(stars);
+
   }
 });
 
